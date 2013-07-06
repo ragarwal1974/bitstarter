38fa9b7bc12c807d58e8200a8933659e7c0ab556
Rekha <ragarwal@adobe.com>
2013-07-06 12:05:46 +0000

    updated web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var infile = "index.html";
+var buffer = new Buffer(50);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFileSync(infile, buffer);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
